d8f76ffb54bde8bef5614b30b48b2644019bcde1
Neil Warner <njwandroid@gmail.com>
2015-03-22 19:22:16 -0500

    Initial commit, hello world + fragment activity plus some strings - commit to set up repo.

---
+++ wear/src/main/java/wordflashcards/njwapps/com/wordflashcards/MainActivity.java
@@ -0,0 +1,25 @@
+package wordflashcards.njwapps.com.wordflashcards;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    private TextView mTextView;
+        //Scratch my own itch, eat my own dogfood
+        // Nice to have watch that does something when you forget your phone or it is out of range instead of almost nothing.
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                mTextView = (TextView) stub.findViewById(R.id.text);
+            }
+        });
+    }
+}
